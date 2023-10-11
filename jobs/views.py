5f45876134425d1917c8663350e366c29e8c3d76
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-10-11 19:48:14 +0200

    Add job_list

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Jobs,Category
 # Create your views here.
+
+class ProductList(generic.ListView):
+    model = Jobs
+
