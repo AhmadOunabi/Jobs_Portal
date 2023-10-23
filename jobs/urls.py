f5c1c23fb20a6bca43be36f21bff5a6f6b707c4e
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-10-23 22:54:24 +0200

    Add registration and login API

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, RegisterAPIVIEW, LoginAPIVIEW
 
 urlpatterns = [
     path('list',ProductList.as_view()),
+    path('api/register',RegisterAPIVIEW.as_view()),
+    path('api/login',LoginAPIVIEW.as_view()),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Jobs,Category
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import exceptions
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
 
 class ProductList(generic.ListView):
     model = Jobs
 
+
+
+class RegisterAPIVIEW(APIView):
+    def post(self,request):
+        data= request.data
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('passwords dont match!')
+        serializer=UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginAPIVIEW(APIView):
+    def post(self,request):
+        email=request.data['email']
+        password=request.data['password']
+        user=User.objects.filter(email=email).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('email or password is incorrect')
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('email or password is incorrect')
+        serializer= UserSerializer(user)
+        return Response(serializer.data)
+        
+        
